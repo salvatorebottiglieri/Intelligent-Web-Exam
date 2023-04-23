23d9b69180e27973d209416d0d4b9ca4474db79e
Salvatore Bottiglieri <sasibottiglieri0@gmail.com>
2023-04-23 14:22:38 +0200

    finish Model class

--- Model.py
+++ Model.py
@@ -20,16 +20,52 @@ class SimilarityGraph:
             self.first_half = self.second_half = 2
 
     def are_connected(self, a: int, b: int) -> bool:
-        return self.graph[a][b] is not np.NINF
-
-    def add_edge(self, s: int, d: int, value: np.float16) -> None:
-        if self.are_connected(s, d):
-            raise Exception(f"There is already an edge between {s} and {d}\n")
-
-        if s <= self.first_half:
-            self.graph[s][d] = value
+        if a <= self.first_half:
+            return self.graph[a][b] is not np.NINF
         else:
-            self.graph[d][s] = value
+            return self.graph[b][a] is not np.NINF
+    
+    def get_edge_value(self, a: int, b: int) -> np.float16:
+        return self.graph[a][b]
+
+    def add_edge(self, a: int, b: int, value: np.float16) -> None:
+        if self.are_connected(a, b):
+            raise Exception(f"There is already an edge between {a} and {b}\n")
+
+        if a <= self.first_half:
+            self.graph[a][b] = value
+        else:
+            self.graph[b][a] = value
 
     def print_graph(self):
         print(self.graph)
+
+
+"""
+This class represents the user-item matrix. It's implemented through a numpy array.
+Because of the ratings' values are between 1 and 5, it uses 16-bit floating point values with
+zeros as value for users that didn't rate an item.
+"""
+
+class UserItemMatrix:
+
+    def __init__(self,first_dimension:int,second_dimension:int)->None:
+        self.first_dimension = first_dimension
+        self.second_dimension = second_dimension
+        self.matrix = np.zeros(shape=(first_dimension,second_dimension),dtype=np.float16)
+
+    def add_value(self,first_index:int,second_index:int,value:np.float16):
+        self.matrix[first_index][second_index] = value
+
+    def get_value(self,first_index:int,second_index:int):
+        return self.matrix[first_index][second_index]
+    
+    def get_row(self,first_index:int):
+        return self.matrix[first_index]
+    
+    def get_column(self,second_index:int):
+        return self.matrix[:,second_index]
+    
+    def print_matrix(self):
+        print(self.matrix)
+        
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from Model import Graph
+import numpy as np
 from Utils import read_dataset
 from WeightsComputation import get_items_in_common
 
 dataset = read_dataset()
 
-#print(dataset["rating"].describe())
+print(dataset["rating"].describe())
+np.savetxt("ratings.csv", dataset["rating"].unique(), delimiter=",")
 
-#dataframe = get_items_in_common(55,54)
-users_cadinality =len(dataset["userId"].unique())
-g = Graph(users_cadinality)
-g.print_graph()
+
 
