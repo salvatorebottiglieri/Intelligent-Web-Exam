968fefd21f273391dacc41ba4c20db8d6431c936
MarcoAruta <marco.aruta3@gmail.com>
2023-05-24 16:47:45 +0200

    cut_time function

--- src/Utils.py
+++ src/Utils.py
@@ -2,12 +2,47 @@ import numpy as np
 import pandas as pd
 import os
 
-
 def read_dataset(dataset_name="ml-latest", filename="ratings.csv") -> pd.DataFrame:
     file_path = os.path.join(os.getcwd(), "Datasets", dataset_name, filename)
     if not os.path.exists(file_path):
         raise Exception("Error, Path to file doesn't exists or file is missing")
     return pd.read_csv(file_path)
+
+def generate_matrix(file_path, num_rows, num_cols):
+    # Read in the CSV file in chunks
+    chunksize = 10000
+    reader = pd.read_csv(file_path, chunksize=chunksize)
+
+    # Create empty sets for unique userIds and movieIds
+    userIds = set()
+    movieIds = set()
+
+    # Iterate over each chunk of the CSV file
+    for chunk in reader:
+        # Add unique userIds and movieIds to their respective sets
+        userIds.update(chunk['userId'].unique())
+        movieIds.update(chunk['movieId'].unique())
+
+    # Convert sets to sorted lists
+    userIds = sorted(list(userIds))
+    movieIds = sorted(list(movieIds))
+
+    # Create empty DataFrame for new matrix
+    newMatrix = pd.DataFrame(index=userIds[:num_rows], columns=movieIds[:num_cols])
+
+    # Read in the CSV file again in chunks
+    reader = pd.read_csv(file_path, chunksize=chunksize)
+
+    # Iterate over each chunk of the CSV file
+    for chunk in reader:
+        # Iterate over each row in the chunk
+        for index, row in chunk.iterrows():
+            # Set the value of the new matrix at the intersection of userId and movieId to the rating
+            if row['userId'] in userIds[:num_rows] and row['movieId'] in movieIds[:num_cols]:
+                newMatrix.at[row['userId'], row['movieId']] = row['rating']
+
+    # Save new matrix to CSV file
+    newMatrix.to_csv('new_matrix.csv')
 
 def rating_time(user:int,item:int,dataset:pd.DataFrame) -> int:
     """
@@ -43,3 +78,6 @@ def max_timestamp(user:int, dataset:pd.DataFrame) -> int:
     """
     timestamps = dataset[dataset['userId'] == user]['timestamp'].values
     return max(timestamps)
+
+def cut_time (min_time, max_time, d):
+    return min_time + ((max_time - min_time) / d)
